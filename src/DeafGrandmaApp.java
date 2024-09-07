28af5fb2324d572140cf9425f4acafde285402ed
sazuko07 <sazu07@gmail.com>
2024-09-07 13:50:51 -0500

    Deaf Grandma 1st Update

---
+++ src/DeafGrandmaApp.java
@@ -0,0 +1,9 @@
+
+public class DeafGrandmaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/GrandmaHearsYou.java
@@ -0,0 +1,9 @@
+
+public class GrandmaHearsYou {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
