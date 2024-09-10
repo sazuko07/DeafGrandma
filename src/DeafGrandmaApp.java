b52909a11de9d65be86cfd75e6b55b76aecea1df
sazuko07 <sazu07@gmail.com>
2024-09-10 12:30:25 -0500

    deaf grandma latest push

--- src/DeafGrandmaApp.java
+++ src/DeafGrandmaApp.java
@@ -2,8 +2,33 @@
 public class DeafGrandmaApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			DeafGrandmaApp app = new DeafGrandmaApp(); 
+				app.launch();
+		}
+		private void launch() {
+			java.util.Scanner scanner = new java.util.Scanner(System.in);
+			String greetGrandma = " ";
+			Grandma Grandma = new Grandma();
+			//Prompt user to say something to grandma, and tell user about grandma's hearing problem
+			
+			System.out.println("Hello, User! You have decided to visit your sweet old grandma.");
+					System.out.println("However, grandma is pretty hard of hearing, and can only hear you when ");
+					System.out.println("you raise your voice (input all caps). If you do not raise your voice, she ");
+					System.out.println("will shout at you with some old grandma nonsense. Nobody wants that, so please ");
+					System.out.println("keep that in mind when you say something to her. You have arrived at grandma's. ");
+					System.out.println("Do you want to say something to your dear old grandma? ");
+					System.out.println();
+					System.out.println("what would you like to say?: ");
+					greetGrandma = scanner.nextLine();
+					Grandma.didGrandmaHearYou(); 
+		}
+			
+			
+			
+				 
+			}
+			
+			
+	
 
-	}
 
-}
--- src/Grandma.java
+++ src/Grandma.java
@@ -1,9 +1,42 @@
 
 public class Grandma {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		public String greetGrandma; 
+		 
+		 //method for when grandma hears you, gives 1 of 5 random responses
+		public void grandmaHearsYou () {
+		 {
+			java.util.Random rand = new java.util.Random();
+			String grandmaResponses[] = new String[5];
+				grandmaResponses[0] = "HEY THERE, SONNY!";
+				grandmaResponses[1] = "WHAT'RE YOU LOOKIN' AT??";
+				grandmaResponses[2] = "IT'S BEEN AGES! WHERE'VE YOU BEEN??";
+				grandmaResponses[3] = "FELLOW?? THERE'S NO FELLOW HERE, NOT SINCE GRANPA DIED ";
+				grandmaResponses[4] = "COME ON IN, SWEETIE! WE HAVE COOKIES!";
+				int index = rand.nextInt(grandmaResponses.length);
+				System.out.println(grandmaResponses[index]); 
+		}}
+		//method for when grandma DOESNT hear you, gives 1 of 5 random responses
+		public void grandmaDoesntHearYou () {
+		
+			java.util.Random rand = new java.util.Random();
+			String grandmaOtherResponses[] = new String[5];
+				grandmaOtherResponses[0] = "WHAT?! SORRY!! YOU'LL HAVE TO SPEAK LOUDER!!";
+				grandmaOtherResponses[1] = "CATS??! WHO'S CATS??!";
+				grandmaOtherResponses[2] = "HEEEEH?? CANT HEAR YOU WITH ALL THESE DAMN FANS!!";
+				grandmaOtherResponses[3] = "NO! I DON'T WANT ANY";
+				grandmaOtherResponses[4] = "DANGIT, BOBBY! WE DONT SELL NATURAL GAS! WE SELL PROPANE!";
+				int secondIndex = rand.nextInt(grandmaOtherResponses.length);
+				System.out.println(grandmaOtherResponses[secondIndex]);
+		}
+		//method to call true/false on whether grandma hears you depending on what input the user gives
+		public boolean didGrandmaHearYou() {
+			if (greetGrandma.equals(greetGrandma.toUpperCase()))
+				didGrandmaHearYou = true; 	
+					grandmaHearsYou();
+			if (!greetGrandma.equals(greetGrandma.toUpperCase()))			
+				didGrandmaHearYou = false;
+					grandmaDoesntHearYou();	
+		}		
 	}
-
-}
+	
+			
